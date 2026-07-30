ad1a84770225af1951152dd05cf6e9b49da68abc
Alessandro <alessandro.pelosi96@gmail.com>
2026-07-30 10:37:45 +0200

    first commit

---
+++ src/main/java/com/generation/SportHub/SportHubApplication.java
@@ -0,0 +1,13 @@
+package com.generation.SportHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportHubApplication.class, args);
+	}
+
+}
